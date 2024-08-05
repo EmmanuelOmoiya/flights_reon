59fcfb6d7a417bbf00448dc1e04b482bcf664d9e
Emmanuel Omoiya <emmanuelomoiya6@gmail.com>
2024-08-05 20:25:39 +0100

    fix: deployment script

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,36 +1,3 @@
-# module Api
-#     module V1
-#         class ReviewsController < ApplicationController
-
-#             def create
-#                 review = Review.new(review_params)
-
-#                 if review.save
-#                     render json: reviewSerializer.new(review).serialized_json
-#                 else
-#                     render json: {error: review.errors.messages}, status: 422
-#                 end
-#             end
-
-#             def delete 
-#                 review = Review.find(params[:id])
-
-#                 if review.destroy
-#                     head no_content
-#                 else 
-#                     render json: {error: review.errors.messages}, status: 422
-#             end
-
-#             private
-
-#             def review_params
-#                 params.require(:review).permit(:title, :description, :score, :review_id)
-#             end
-#         end
-#     end
-# end
-
-
 module Api
     module V1
       class ReviewsController < ApplicationController
